7b61659017e1839865f6e8abd899ef772dd5e913
Renan <renan.fachin@hotmail.com>
2023-04-06 16:08:26 -0300

    Utilizando react-hook-form - REGISTER

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
 import './styles/global.css'
 
+import { useForm } from 'react-hook-form'
+
 export function App() {
+  const { register } = useForm()
+
   return (
-    <main className="h-screen bg-zinc-50">
-      <form action="">
-        <label htmlFor="">E-mail</label>
-        <input type="email" name="email" />
+    <main className="h-screen bg-zinc-950 text-zinc-300 flex items-center justify-center">
+      <form className="flex flex-col gap-4 w-full max-w-xs">
+        <div className="flex flex-col gap-1">
+          <label htmlFor="">E-mail</label>
+          <input
+            type="email"
+            className="border border-zinc-200 shadow-sm rounded h-10 px-3"
+            {...register('email')}
+          />
+        </div>
 
-        <label htmlFor="">Senha</label>
-        <input type="password" name="password" />
+        <div className="flex flex-col gap-1">
+          <label htmlFor="">Senha</label>
+          <input
+            type="password"
+            className="border border-zinc-200 shadow-sm rounded h-10 px-3"
+            {...register('password')}
+          />
+        </div>
 
-        <button type="submit">Salvar</button>
+        <button
+          type="submit"
+          className="bg-emerald-500 rounded font-semibold text-white h-10 hover:bg-emerald-600"
+        >
+          Salvar
+        </button>
       </form>
     </main>
   )
