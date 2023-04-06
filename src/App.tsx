0ac6569fc3696c5b4b35ed0c53c89c444ef0b50e
Renan <renan.fachin@hotmail.com>
2023-04-06 16:14:33 -0300

    Utilizando react-hook-form - HANDLESUBMIT

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,23 @@ import './styles/global.css'
 import { useForm } from 'react-hook-form'
 
 export function App() {
-  const { register } = useForm()
+  const { register, handleSubmit } = useForm()
+
+  function createUser(data: any) {
+    console.log(data)
+  }
 
   return (
     <main className="h-screen bg-zinc-950 text-zinc-300 flex items-center justify-center">
-      <form className="flex flex-col gap-4 w-full max-w-xs">
+      <form
+        onSubmit={handleSubmit(createUser)}
+        className="flex flex-col gap-4 w-full max-w-xs"
+      >
         <div className="flex flex-col gap-1">
           <label htmlFor="">E-mail</label>
           <input
             type="email"
-            className="border border-zinc-200 shadow-sm rounded h-10 px-3"
+            className="border border-zinc-800 shadow-sm rounded h-10 px-3 bg-zinc-900"
             {...register('email')}
           />
         </div>
@@ -21,7 +28,7 @@ export function App() {
           <label htmlFor="">Senha</label>
           <input
             type="password"
-            className="border border-zinc-200 shadow-sm rounded h-10 px-3"
+            className="border border-zinc-800 shadow-sm rounded h-10 px-3 bg-zinc-900"
             {...register('password')}
           />
         </div>
