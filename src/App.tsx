49fb33b667c00260e12e266987fa9e0b14b796ac
Renan <renan.fachin@hotmail.com>
2023-04-06 16:25:27 -0300

    Cria objeto de validação com ZOD

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import './styles/global.css'
 
 import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+
+const createUserFormSchema = z.object({
+  email: z
+    .string()
+    .nonempty('O e-mail é obrigatório')
+    .email('Formato de e-mail inválido'),
+  password: z.string().min(6, 'A senha precisa de no mínimo 6 caracteres'),
+})
 
 export function App() {
   const { register, handleSubmit } = useForm()
